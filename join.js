31e8b79af9f78da46c21366be24a021eb42f08dc
Alyssa Cooper <alyssarcoops@gmail.com>
2017-10-02 15:39:10 -0400

    join.js
    join example

---
+++ join.js
@@ -0,0 +1,7 @@
+//joins each element of the array
+
+var splitArray = ["Join","all","of","the","elements", "in", "this", "array", "to", "make", "a", "sentence"];
+var joinArray = splitArray.join(" ");
+console.log(joinArray);
+
+// join is a simple method used in functional programming to combine the contents of an array
